0685dfcb238b19fb3afcb6d1b4b02b71ae266f47
peaky76 <robertjamespeacock@gmail.com>
2020-07-28 17:39:15 +0100

    conversion and formatting

--- src/app.js
+++ src/app.js
@@ -5,9 +5,20 @@ document.addEventListener("DOMContentLoaded", () => {
     el: "#app",
     data: {
       rates: [],
-      unitsToConvert: 0,
+      unitsToConvert: 1,
+      // selectedCurrency: null,
+      conversionRate: 1,
     },
-    computed: {},
+    computed: {
+      unitsNewCurrency() {
+        return this.convert(this.unitsToConvert, this.conversionRate);
+      },
+    },
+    filters: {
+      currencyFormatter(value) {
+        return (Math.round(value * 100) / 100).toFixed(2);
+      },
+    },
     mounted() {
       this.fetchRates();
     },
@@ -16,6 +27,9 @@ document.addEventListener("DOMContentLoaded", () => {
         fetch("https://api.exchangeratesapi.io/latest")
           .then((response) => response.json())
           .then((data) => (this.rates = data.rates));
+      },
+      convert(baseUnits, rate) {
+        return baseUnits * rate;
       },
     },
   });
